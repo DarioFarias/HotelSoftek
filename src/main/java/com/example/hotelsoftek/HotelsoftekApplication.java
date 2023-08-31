fcb31d81e8eea35375f0fe4de1a3dd7c5209ad66
Dario Farias <dajfarias@gmail.com>
2023-08-31 14:42:05 -0600

    primer commit

---
+++ src/main/java/com/example/hotelsoftek/HotelsoftekApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelsoftek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsoftekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsoftekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotelsoftek/HotelsoftekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelsoftek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsoftekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
